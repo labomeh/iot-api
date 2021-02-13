599e534b5004c250eccda7f43504bdba77e5a4b2
Thomas Broutier <thomas.broutier@gmail.com>
2021-02-13 12:53:54 +0100

    feature: Updated GET ratios 2

--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -13,7 +13,7 @@ dataRouter
         const weatherData = {
           fog: (normalizeData(temperature, 19, 22, true)) * normalizeData(humidity, 45, 70),
           rain:  (1 - normalizeData(temperature, 16, 20, true)) * normalizeData(humidity, 35, 70),
-          snow: (1 - normalizeData(temperature, 10, 17)) * normalizeData(humidity, 30, 70),
+          snow: (1 - normalizeData(temperature, 13, 17)) * normalizeData(humidity, 30, 70),
           temperature: normalizeData(temperature, 10, 35),
           light: normalizeData(lux, 30, 110),
         };
